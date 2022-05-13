309e309fd3d81611d0591aef1e02ce916c380bd2
BreensRobbe <robbe.breens@student.howest.be>
2022-05-13 15:38:29 +0200

    Initial commit, api fetch and basic data visualization

---
+++ script/network.js
@@ -0,0 +1,18 @@
+const serverEndPoint = 'https://api.nasa.gov/planetary/apod'
+const apiKey = '?api_key=MsVwdNzUZ5FpCweL2JX7nbFQz9YbvTO1WugLvjqG'
+
+
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+
+
+    const response = await fetch(serverEndPoint + apiKey)
+    const responseJson = await response.json()
+    console.log(responseJson)
+
+    document.querySelector('.date').textContent = responseJson["date"]
+    document.querySelector('.explanation').textContent = responseJson["explanation"]
+    document.querySelector('.title').textContent = responseJson["title"]
+    document.querySelector('.image').src= responseJson["url"]
+})
